84b8dc0af4b1197997922d8638a981718ae2b691
KaBow <51200778+KaBow@users.noreply.github.com>
2020-07-26 23:50:31 +0800

    Update

--- src/main/java/com/itcat/TestReOrderArray.java
+++ src/main/java/com/itcat/TestReOrderArray.java
@@ -1,15 +1,15 @@
 package com.itcat;
 
 import java.util.Arrays;
-import java.util.HashMap;
 
 /**
  * 调换数组中奇数和偶数的位置，使所有奇数在前，偶数在后，并不改变元素相对位置
  */
 public class TestReOrderArray {
     public static void main(String[] args) {
-        int[] array = {2,3,1,5,7,8,4};
+        int[] array = {2,3,1,5,7,0,4,9,11,34,87};
         reOrderArray1(array);
+        reOrderArray2(array);
     }
     //
     public static void reOrderArray1(int [] array) {
@@ -32,6 +32,35 @@ public class TestReOrderArray {
                 array[t] = list[i];
                 t++;
             }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+    //省去一次遍历
+    public static void reOrderArray2(int [] array) {
+        int pre = 0;
+        int col = array.length - 1;
+        int[] list = new int[array.length];
+        if(array.length == 0||array == null){
+            System.out.println(Arrays.toString(array));
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]%2 != 0){
+                list[pre] = array[i];
+                pre++;
+            }else {
+                list[col] = array[i];
+                col--;
+            }
+        }
+        col = array.length - 1;
+        for (int i = 0; i < array.length; i++) {
+            if(list[i]%2 != 0){
+                array[i] = list[i];
+            }else {
+                array[i] = list[col];
+                col--;
+            }
+
         }
         System.out.println(Arrays.toString(array));
     }
